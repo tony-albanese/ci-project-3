e6021a4f43d89c3e5945c26cbf6341ed5202ae98
tony <tony.albanese2000@gmail.com>
2022-12-10 17:15:35 +0100

    Add method to calculate free energy from data.

--- repository/thermodynamics.py
+++ repository/thermodynamics.py
@@ -96,8 +96,8 @@ prods = getProducts()
 
 def calculate_enthalpy_change(data_frame: pandas.core.frame.DataFrame, reactants: list, products: list):
     column_name = 'dH'
-    sum_products = 0;
-    sum_reactants = 0;
+    sum_products = 0
+    sum_reactants = 0
 
     for reactant in reactants:
         row = reactant[0]
@@ -121,8 +121,30 @@ def calculate_enthalpy_change(data_frame: pandas.core.frame.DataFrame, reactants
 
 
 
-def calculateFreeEnergy():
-    pass
+def calculate_free_energy(data_frame: pandas.core.frame.DataFrame, reactants: list, products: list):
+    column_name = 'dG'
+    sum_products = 0
+    sum_reactants = 0
+
+    for reactant in reactants:
+        row = reactant[0]
+        coefficient = reactant[1]
+        enthalpy = data_frame._get_value(row, column_name)
+        sum_reactants = sum_reactants + enthalpy*coefficient
+
+    for product in products:
+        row = product[0]
+        coefficient = product[1]
+        enthalpy = data_frame._get_value(row, column_name)
+        sum_products = sum_products + enthalpy*coefficient
+
+    energy_change = sum_products - sum_reactants
+
+    print("Sum products: ")
+    print(sum_products)
+    print("Sum of reactants:")
+    print(sum_reactants)
+    return energy_change
 
 
 def calculateEntropy():
