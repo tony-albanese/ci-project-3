45e958f0bd5f0545d1276f83a79618e8b32a485a
tony <tony.albanese2000@gmail.com>
2022-12-12 11:50:48 +0100

    Initial commit - added some widgets

--- ui/chem_ui.py
+++ ui/chem_ui.py
@@ -1,12 +1,33 @@
 from textual.app import App, ComposeResult
 from textual.containers import Container
 from textual.reactive import reactive
-from textual.widgets import Button, Header, Footer, Static
+from textual.widgets import Button, Header, Footer, Static, Placeholder
 
 class ChemApp(App):
+    CSS_PATH = "chem_ui.css"
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
+        yield InputArea()
+        yield  MainPanel()
 
 
+class InstructionsWindow(Static):
+    def compose(self) -> ComposeResult:
+        yield Placeholder()
 
+class DataWindow(Static):
+    def compose(self) -> ComposeResult:
+        yield Placeholder()
+
+class OutputWindow(Static):
+    def compose(self) -> ComposeResult:
+        yield Placeholder()
+
+class MainPanel(Static):
+    def compose(self) -> ComposeResult:
+        yield Container(InstructionsWindow(), DataWindow(), OutputWindow())
+
+class InputArea(Static):
+    def compose(self) -> ComposeResult:
+        yield Placeholder()
