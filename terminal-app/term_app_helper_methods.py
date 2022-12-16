b80ed9d6f4e4b6c11073675ecc45ce7ecf38934d
tony <tony.albanese2000@gmail.com>
2022-12-16 08:48:18 +0100

    refactor path of data set for file load.

--- terminal-app/term_app_helper_methods.py
+++ terminal-app/term_app_helper_methods.py
@@ -5,7 +5,7 @@ from rich import print
 import re
 
 def load_data_frame():
-    dataFrame = pd.read_csv('data/molecule_data.csv')
+    dataFrame = pd.read_csv('molecule_data.csv')
     dataFrame['dH'] = dataFrame['dH'].astype(float)
     return dataFrame
 
