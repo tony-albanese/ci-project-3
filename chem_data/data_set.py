cdd90331b420c1cd96d94ea5f8a7a06ca94c6153
tony <tony.albanese2000@gmail.com>
2022-12-19 11:25:45 +0100

    Refactor variable name PEP8

--- chem_data/data_set.py
+++ chem_data/data_set.py
@@ -1,6 +1,8 @@
 import pandas as pd
+
+
 def load_data_frame():
     pd.set_option('display.max_rows', None)
-    dataFrame = pd.read_csv('data/molecule_data.csv')
-    dataFrame['dH'] = dataFrame['dH'].astype(float)
-    return dataFrame
+    data_frame = pd.read_csv('data/molecule_data.csv')
+    data_frame['dH'] = data_frame['dH'].astype(float)
+    return data_frame
